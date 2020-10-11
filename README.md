8a91daa52e0ce512abe8eb13a1457ed1489954e2
Walter Hoerman <whoerman@gmail.com>
2020-10-11 12:52:43 -0400

    changing to javascript program

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# BeerHelper
+# 56Dennett
---
+++ navbar.js
@@ -0,0 +1,16 @@
+
+    let navheader = '<nav class="navbar navbar-light bg-light"> \
+<a href="./index.html"><span class="navbar-brand mb-0 h1"><i class="fas fa-beer"></i> Beer Helper  \
+    <Dlg></Dlg> \
+  </span></a> \
+<ul class="nav text-center"> \
+  <li class="nav-item"> \
+    <a class="nav-link" href="./index.html">Home</a> \
+  </li> \
+</ul> \
+</nav>'
+
+
+
+document.getElementById("navbardiv").innerHTML +=
+navheader;
