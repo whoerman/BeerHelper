db9be72e2a3110a0aaf60812ead0a199a02e9891
Walter Hoerman <whoerman@gmail.com>
2020-10-15 16:02:11 -0400

    adding glass types images

--- glassTypedataArray.js
+++ glassTypedataArray.js
@@ -2,67 +2,72 @@
 let glassTypeData = [
     {
         type: 'Footed Pilsner',
-        image: '',
+        image: 'germanpils.jpg',
         country: 'German',
         description: 'May have a longer stem. Sutiable for German and Czech/Bohemian Pilsners.',
     },{
         type: 'Mass',
-        image: '',
+        image: 'munichhelles.jpg',
         country: 'German',
         description: 'Liter-sized glass associated with beer gardens and Oktoberfest. Can be used for any lager less than 6% ABV.',
     },{
         type: 'Weizen Vase',
-        image: '',
+        image: 'dunlesweissbier.jpg',
         country: 'German',
         description: 'Accomodates half liter of beer plus sizeable head. Curvier versions seen.',
     },{
         type: 'Stange',
-        image: '',
+        image: 'kolsch.jpg',
         country: 'German',
         description: 'Used to serve Kölsch, often in 0.2 liter size.',
     },{
         type: 'Willi Becher',
-        image: '',
+        image: 'viennalager.jpg',
         country: 'German',
         description: 'Classic shape suitable for all European lagers.',
     },{
         type: 'Shaker Glass',
-        image: '',
+        image: 'americanlager.jpg',
         country: 'Alerican',
         description: 'American cocktail shaker adapted for beer. Can be used for any American style of reasonable alcohol content.',
     },{
         type: 'Pub Mug',
-        image: '',
+        image: 'bestbitter.jpg',
         country: 'English',
         description: 'The dimpled mug has a slight retro feel. Used for any classic British of Irish ale.',
     },{
         type: 'Nonic Pint',
-        image: '',
+        image: 'englishipa.jpg',
         country: 'English',
         description: 'Every day pub glass suited to any British or Irish ale.',
     },{
         type: 'Tulip Pint',
-        image: '',
+        image: 'irishstout.jpg',
         country: 'English',
         description: 'Commonly associated with Irish Stout but may be used for any British or Irish ale.',
     },{
+        type: 'Thistle',
+        image: 'scottishexport.jpg',
+        country: 'English',
+        description: 'Commonly associated with Scottish ale.',
+    },{
         type: 'Goblet',
-        image: '',
+        image: 'belgiandarkstrongale.jpg',
         country: 'Belgian',
         description: 'Heavier chalice shape used for some trappist & Abbey ales and some Belgian pale ales.',
     },{
         type: 'Tulip',
-        image: '',
+        image: 'saison.jpg',
         country: 'Belgian',
         description: 'Good utility glass suited to many Belgian styles from saison to blond ales.',
     },{
         type: 'French Jelly Glass',
-        image: '',
+        image: 'gueuze.jpg',
         country: 'Belgian',
         description: 'Associated with Witbier but similar shapes used for lambics, Flanders red, etc.',
     },{
         type: 'Snifter',
-        image: '',
+        image: 'eisbock.jpg',
         country: 'Belgian',
         description: 'Smaller glass for higher alcohol beers across all cultures: barleywines, eisbocks, imperial stouts, barrel-aged beers, etc.',
     }, ]
