71bce8f70e721ca8232fc58eef01a98d684fb837
Walter Hoerman <whoerman@gmail.com>
2020-10-16 11:10:36 -0400

    fixing graphics

--- main.js
+++ main.js
@@ -4,12 +4,12 @@
             '<div class="card border-secondary text-center"> \
         <div class="card-body"> \
             <h4 class="text-center m-0">' + (i + 1) + '.   <i class="fas fa-Beer"></i> ' + beerData[i].type + ' </h4>\
-            <img class="card-img-top" src="./assets/images/beerpics/' + beerData[i].image + '" alt="' + beerData[i].type + ' picture"> \
             <p class="text-center m-0"><em>' + beerData[i].country + '</em></p>\
             <p class="card-title">Color: ' + beerData[i].color.light + ' to ' + beerData[i].color.dark + ' (' + beerData[i].color.lightSRM + ' to ' + beerData[i].color.darkSRM + ' SRM)</p>\
             <p class="card-title">' + beerData[i].bitter.type + ' Bitterness (' + beerData[i].bitter.lowibu + ' to ' + beerData[i].bitter.highibu + ' IBU)</p>\
             <p class="card-title">' + beerData[i].alcohol.lowerdesc + ' to ' + beerData[i].alcohol.higherdesc + ' Alcohol </p>\
             <p class="card-title">(' + beerData[i].alcohol.lowerabv + '% to ' + beerData[i].alcohol.higherabv + '% ABV)</p>\
+            <img class="card-img-top float-right" src="./assets/images/beerpics/' + beerData[i].image + '" alt="' + beerData[i].type + ' picture"> \
             <p class="card-title">' + beerData[i].flavor + '</p>\
             <p class="card-title"><em>' + beerData[i].history + '</em></p>\
         </div> \
@@ -23,9 +23,9 @@
             '<div class="card border-secondary text-center"> \
             <div class="card-body"> \
                 <h4 class="text-center m-0"> Glassware Type: </h4>\
-                <img class="card-img-top" src="./assets/images/beerpics/' + glassTypeData[i].image + '" alt="' + beerData[i].type + ' picture"> \
                 <h4 class="text-center m-0">' + (i + 1) + '.   <i class="fas fa-Beer"></i> ' + glassTypeData[i].type + ' </h4>\
                 <p class="text-center m-0"><em>' + glassTypeData[i].country + '</em></p>\
+                <img class="card-img-top float-right" src="./assets/images/beerpics/' + glassTypeData[i].image + '" alt="' + beerData[i].type + ' picture"> \
                 <p class="card-title">' + glassTypeData[i].description + '</p>\
             </div> \
         </div>';
@@ -114,7 +114,7 @@
     }
 
     // listAll();
-    // listGlassAll();
-    listFlavorQuestionAll();
+    listGlassAll();
+    // listFlavorQuestionAll();
     // listHistoryQuestionAll();
     // listProfileQuestionAll();
