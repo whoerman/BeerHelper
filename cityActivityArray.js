89a7fea50e0ba168fd6a1480a52a9db3a707c1fb
Walter Hoerman <whoerman@gmail.com>
2020-10-17 09:15:22 -0400

    adding city data

--- cityActivityArray.js
+++ cityActivityArray.js
@@ -1,14 +1,75 @@
 //setting the data for all the beer types
-let cityActivityData = [{
-        parameter: "lowest ABV",
-        activity: {
-            beginning: 
-               [ "When shooting pub darts in ", "When arguing policics in ", "When matching pints in ", ]
-        }
-            ["When shooting pub darts in ", ", which of these is LEAST likely to make you miss the bullseye?"];
-            ["When arguing policics in ", ", which of these is LEAST likely to make you get in a fight?"],
-            ["When matching pints in ", ", which of these is LEAST likely to make you fall off your stool?"],
-        ],
-    };
+let cityData = [{
+    country: 'Belgian',
+    cities: [
+        "Brussels", "Bruges", "Ghent", "Antwerp", "Charleroi", "Mons", "Liège", "Schaerbeek", "Anderlecht", "Leuven"
+    ]
+}, {
+    country: 'English, Scottish & Irish',
+    cities: [
+        "London", "Liverpool", "Yorkshire", "Newcastle", "Edinburgh", "Oban", "Glasgow", "Aberdeen", "Dublin", "Belfast", "Limerick", "Galway"
+    ]
+}, {
+    country: 'German & Czech',
+    cities: [
+        "Berlin", "Frankfurt", "Cologne", "München", "Hamburg", "Stuttgart", "Düsseldorf", "Leipzig", "Prague", "Cesky Krumlov", "Brno", "Ostrava"
+    ]
+}, {
+    country: 'American',
+    cities: [
+        "Boston", "New York", "Miami", "Chicago", "Los Angeles", "Seattle", "San Antonio", "Denver", "Phoenix", "Baltimore"
+    ]
+},{
+    country: 'Other',
+    cities: [
+        "Tokyo", "Sydney", "Moscow", "Aukland", "Singapore", "Saigon", "Beijing"
+    ]
+}, ]
 
-]
+//setting the data for all the beer types
+let activityData = [{
+    parameter: "Lowest ABV",
+    activity: {
+        beginning: ["When shooting pub darts in ",
+            "When arguing policics in ",
+            "When matching pints in ",
+            "For relaxing around a barbque in ",
+        ],
+        ending: [", which of these is LEAST likely to make you miss the bullseye?",
+            ", which of these is LEAST likely to make you get in a fight?",
+            ", which of these is LEAST likely to make you fall off your stool?",
+            ", which of these beers are the most sessionable?",
+        ],
+    },
+
+}, {
+    parameter: "Highest ABV",
+    activity: {
+        beginning: ["ing pubWhen shoot darts in ",
+            "When arguing policics in ",
+            "When matching pints in ",
+            "For relaxing around a barbque in ",
+        ],
+        ending: [", which of these is MOST likely to make you miss the board?",
+            ", which of these is MOST likely to make you punched in the nose?",
+            ", which of these is MOST likely to make you knock over your beer?",
+            ", which of these beers is MOST likely to make you not be able to get out of your campchair?",
+        ],
+    },
+
+}, {
+    parameter: "",
+    activity: {
+        beginning: ["",
+            "",
+            "",
+            "",
+        ],
+        ending: [",",
+            ", ",
+            ", ",
+            ", ",
+        ],
+    },
+
+},  ]
